89260f51e35111d0e696c52dc8aefb512d190f84
aryanAKN27 <142150283+AK-Developers@users.noreply.github.com>
2023-11-28 11:45:28 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,6 @@
 <?php
 $myfile = fopen("location.txt", "w")
-$txt = "Lat: ".$_GET["Latitude"] . "\nLong: ". $_GET["Longitude"];
+$txt = "Lat: ".$_GET["lat"] . "\nLong: ". $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
