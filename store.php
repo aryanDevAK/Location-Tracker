9abf6292d976450039923df7b39f89b2eea2c080
aryanAKN27 <142150283+aryanDevAK@users.noreply.github.com>
2023-12-24 12:38:41 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,11 @@
 <?php
-$myfile = fopen("location.txt", "w");
-$txt = "Lat: ".$_GET["lat"] . "\nLong: ". $_GET["long"]. "\nIP :" . $_SERVER["REMOTE_ADDR"];
+$lat = $_GET["lat"] ?? "Not provided"; // Handle missing lat parameter
+$long = $_GET["long"] ?? "Not provided"; // Handle missing long parameter
+
+$myfile = fopen("location.txt", "a"); // Append to the file
+$txt = "Lat: " . $lat . "\nLong: " . $long . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\n\n"; // Add newlines for better formatting
 fwrite($myfile, $txt);
 fclose($myfile);
+
+echo "Location saved successfully!"; // Provide feedback to the user
 ?>
